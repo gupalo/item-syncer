4af4da758088ceb9d58cc4203ff0e72e2d55ec30
vl <vladimir.luchaninov@gmail.com>
2023-01-18 20:17:43 +0100

    fix tests

--- tests/ItemSyncer/ItemSyncerTest.php
+++ tests/ItemSyncer/ItemSyncerTest.php
@@ -25,6 +25,16 @@ class ItemSyncerTest extends TestCase
         self::assertSame(3, $diff->keptItems[3]->getId());
     }
 
+    public function testDiffKeepingAndUpdateItem(): void
+    {
+        $local = [$this->item(1, 'old'), $this->item(3)];
+        $remote = [$this->item(1, 'new'), $this->item(2)];
+
+        $diff = (new ItemSyncer())->diffKeeping($remote, $local);
+
+        self::assertSame('new', $diff->updatedItems[1]->getName());
+    }
+
     public function testDiffArchiving(): void
     {
         $local = [$this->item(1), $this->item(3)];
@@ -59,8 +69,8 @@ class ItemSyncerTest extends TestCase
         self::assertSame(3, $diff->removedItems[3]->getId());
     }
 
-    public function item(int $id): FakeSyncableEntity
+    public function item(int $id, string $name = ''): FakeSyncableEntity
     {
-        return new FakeSyncableEntity($id);
+        return new FakeSyncableEntity($id, $name);
     }
 }
